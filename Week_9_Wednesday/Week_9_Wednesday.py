c1fb3f373dda040e88ee10c2e5547f87b8525284
Joseph Holmes <joseph.holmes97@yahoo.co.uk>
2023-03-08 18:48:51 +0000

    Added some simple code to get an animal and question from the user

--- Week_9_Wednesday/Week_9_Wednesday.py
+++ Week_9_Wednesday/Week_9_Wednesday.py
@@ -1,8 +1,62 @@
 
 # Animal guessing game
 
-print("-------------------Animal Guessing Game-------------------")
-print("\nPlease think of an animal....")
+#----------------------------------------------------------------Classes----------------------------------------------------------------
 
-input("When you are ready, please press enter to begin.......\n")
+class Animal: # Class for animals
+    
+    mName = ""
+    m_Questions = list()
 
+    def __init__(self, sName):
+        self.mName = sName
+
+    def AddNewQuestion(self, sQuestion): # Function for adding new questions
+
+        self.m_Questions.append(sQuestion)
+
+#----------------------------------------------------------------Classes----------------------------------------------------------------
+
+#----------------------------------------------------------------Functions----------------------------------------------------------------
+
+def ProgramStarter(s_GuessedAnimals):
+
+    print("\nPlease think of an animal....")
+
+    input("When you are ready, please press enter to begin.......\n")
+
+    sAnimalName = input("\nPlease enter the name of your first animal: \n")
+
+    s_GuessedAnimals.append(Animal(sAnimalName))
+
+    Animal.AddNewQuestion("Please enter a question to which the answer could be for a " + s_GuessedAnimals[0].mName + ": \n")
+
+    return s_GuessedAnimals
+
+def MakeAGuess(s_GuessedAnimals):
+
+    return s_GuessedAnimals
+
+#----------------------------------------------------------------Functions----------------------------------------------------------------
+
+#----------------------------------------------------------------Main Function----------------------------------------------------------------
+def Main(): # Function for the main body of code
+
+    bContinue = True
+    s_GuessedAnimals = list()
+
+    while bContinue == True:
+
+        print("-------------------Animal Guessing Game-------------------")
+
+        if len(s_GuessedAnimals) == 0:
+            s_GuessedAnimals = ProgramStarter(s_GuessedAnimals)
+
+        else:
+            MakeAGuess(s_GuessedAnimals)
+
+#----------------------------------------------------------------Main Function----------------------------------------------------------------
+
+#-------------------------------------------------------------Code Starts Running-------------------------------------------------------------
+
+Main() # Run the main body of code
