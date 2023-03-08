a43f9c3243835e990dd47aa79f75cdb054ca7d89
Joseph Holmes <joseph.holmes97@yahoo.co.uk>
2023-03-08 17:37:54 +0000

    Started program, created start message

--- Week_9_Wednesday/Week_9_Wednesday.py
+++ Week_9_Wednesday/Week_9_Wednesday.py
@@ -1,1 +1,8 @@
 
+# Animal guessing game
+
+print("-------------------Animal Guessing Game-------------------")
+print("\nPlease think of an animal....")
+
+input("When you are ready, please press enter to begin.......\n")
+
